85411d9c3f091815f794a856960afd9ab8d01ba9
yutaf <yutafuji2008@gmail.com>
2015-07-21 18:45:56 +0900

    Remove defineParse property

--- DbRepository.php
+++ DbRepository.php
@@ -11,7 +11,6 @@ abstract class DbRepository
     protected $dbManager;
     protected $table_name;
     public $columns;
-    protected $defineParse;
 
     abstract protected function setTableName();
 
@@ -27,7 +26,6 @@ abstract class DbRepository
         $this->dbManager = $dbManager;
         $this->setTableName();
         $this->setColumns();
-        $this->defineParse = new DefineParse();
     }
 
     /**
